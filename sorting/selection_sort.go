ca17c40c67e6139446d3fb7da623b0975708265f
Vardana Bhanot <bhanotvardana@gmail.com>
2020-01-13 19:26:50 +0530

    Another change

--- sorting/selection_sort.go
+++ sorting/selection_sort.go
@@ -1,28 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
 func main() {
 	var array []int
 
-	array = []int{23, 65, 78, 90, 12, 4,90,3,1}
+	array = []int{23, 65, 78, 90, 12, 4, 90, 3, 1}
 
 	for i := 0; i < len(array)-1; i++ {
-		position:= i
-		for j := i+1; j < len(array); j++ {
-			if array[position] > array[j] {
-				position = j
-				
+		minValue := i
+		for j := i + 1; j < len(array); j++ {
+			if array[minValue] > array[j] {
+				minValue = j
 			}
-			if position != i{
-				swap:=array[i];
-				array[i]=array[position];
-				array[position]=swap;
+			if minValue != i {
+				swap := array[i]
+				array[i] = array[minValue]
+				array[minValue] = swap
 			}
 		}
 		fmt.Println(array)
 	}
-	
+
 }
